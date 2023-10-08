7d58b6c92cc73c2e8d6f3177a9acfcb1a260be65
VIRGINIE  ROSA_ARSENEV <virginieralg@gmail.com>
2023-10-08 14:01:17 +0200

    adding form & thank.php

---
+++ form.php
@@ -0,0 +1,37 @@
+
+<form  action="/thanks.php"  method="post">
+    <div>
+      <label  for="nom">Nom :</label>
+      <input  type="text"  id="nom"  name="user_name">
+    </div>
+
+    <div>
+      <label  for="prénom">Prénom :</label>
+      <input  type="text"  id="prénom"  name="user-prénom">
+    </div>
+    <div>
+      <label  for="email">E-mail :</label>
+        <input  type="email"  id="email"  name="user_email">
+    </div>
+    <div>
+      <label  for="télephone">Téléphone :</label>
+        <input  type="int"  id="téléphone"  name="user-téléphone">
+    </div>
+    <div>
+      <label  for="sujet">Sujet :</label>
+        <select  id="sujet"  name="user-sujet">
+            <option value="demande d'information">demande d'information</option>
+            <option value="demande de rdv">demande de rdv</option>
+            <option value="demande de devis">demande de devis</option>
+        </div>
+   
+    <div>
+      <label  for="message">Message :</label>
+      <textarea  id="message"  name="user_message"></textarea>
+    </div>
+    <div  class="button">
+      <button  type="submit">Envoyer votre message</button>
+    </div>
+  </form>
+  
+  
---
+++ thanks.php
@@ -0,0 +1,5 @@
+Merci {Prénom} {Nom} de nous avoir contacté à propos de “{sujet}”.
+
+Un de nos conseillers vous contactera soit à l’adresse {e-mail} ou par téléphone au {téléphone} dans les plus brefs délais pour traiter votre demande : 
+
+{message}
